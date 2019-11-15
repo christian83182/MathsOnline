b57e96c46b30be8821dd979e199132a7b08c9805
Christian Folkesson <christian.uaf@gmail.com>
2019-11-15 18:47:02 +0000

    Initial commit.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <v-app-bar app color="blue" clipped-left>
+            <v-app-bar-nav-icon @click="drawerOpen = !drawerOpen"></v-app-bar-nav-icon>
+            <v-toolbar-title>
+                <span class="heading mr-2">Math Online</span>
+                <span class="font-weight-thin heading">Digital Problem Generator</span>
+            </v-toolbar-title>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawerOpen" app clipped>
+            <v-list>
+
+                <v-list-item link>
+                    <v-list-item-action> <v-icon>mdi-alert-circle</v-icon> </v-list-item-action>
+                    <v-list-item-content> <v-list-item-title>Nothing here yet!</v-list-item-title> </v-list-item-content>
+                </v-list-item>
+
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar",
+        data(){
+            return {
+                drawerOpen: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <span class="display-1 ma-12">Hello World</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
