93d2019e752e3b229f8716daee871060b9459150
Christian Folkesson <christian.uaf@gmail.com>
2019-11-16 14:59:31 +0000

    + Added a timeline to display the application's changelog. This is automatically fetched from the applications github repository. - Removed dummy page from changelog.

--- src/views/Changelog.vue
+++ src/views/Changelog.vue
@@ -1,12 +1,53 @@
 <template>
-    <div class="display-1 font-weight-thin ma-6 text-center">
-        Changelog: There's nothing here yet... but routing works!
-    </div>
+    <v-container class="fill-height">
+        <v-row align="center" justify="center">
+            <v-col cols="">
+
+                <div v-if="commitHistoryResponse === null">
+                    <v-skeleton-loader  class="ma-2" type="article" v-for="index in 3" :key="index"></v-skeleton-loader>
+                </div>
+
+                <v-timeline dense v-if="this.commitHistoryResponse !== null">
+                    <v-timeline-item v-for="(commit, index) in this.commitHistoryResponse" :key="index">
+                        <v-card class="mr-6">
+                            <v-card-title class="subtitle-1 py-2 px-4">
+                                Change #{{ commitHistoryResponse.length - index }}
+                            </v-card-title>
+                            <v-card-text class="pb-3 px-6">
+                                {{ commit.commit.message }}
+                            </v-card-text>
+                        </v-card>
+                    </v-timeline-item>
+                </v-timeline>
+
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: "Changelog"
+        name: "Changelog",
+        data(){
+            return{
+                commitHistoryResponse: null,
+                loading: true
+            }
+        },
+        methods:{
+            fetchCommitHistory(){
+                fetch(
+                    "https://api.github.com/repos/christian83182/MathsOnline/commits"
+                ).then(response => {
+                    return response.json();
+                }).then(jsonData => {
+                    this.commitHistoryResponse = jsonData;
+                });
+            }
+        },
+        mounted(){
+            this.fetchCommitHistory();
+        }
     }
 </script>
 
