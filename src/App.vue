da2a856cc3e6d4e547ee349e9e6641e42d68ebe3
Christian Folkesson <christian.uaf@gmail.com>
2019-11-16 02:27:07 +0000

    Added a section on the menu for most future features. Added light/dark theme toggle from rollover menu. Menu is now constructed from data. Added footer.

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
     <v-content>
       <router-view></router-view>
     </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 export default {
   name: "App",
-  components: {NavBar},
+  components: {Footer, NavBar},
   data: () => ({
     //
   })
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer padless>
+        <v-col class="text-center font-weight-thin" cols="12">
+            Copyright {{ new Date().getFullYear() }}. All rights reserved.
+        </v-col>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
