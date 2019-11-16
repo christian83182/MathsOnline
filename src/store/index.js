7f179772aa56038f35ce56af8de16720eb7fc2ef
Christian Folkesson <christian.uaf@gmail.com>
2019-11-16 12:28:51 +0000

    Extracted the menu items into the applications's state for reuse in other components. Menu items now have a description

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,52 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    topics:[{
+      name: "Basic Arithmetic",
+      description: "Basic arithmetic involves the application of basic numerical operators such as addition, subtraction, multiplication, and division.",
+      color:"light-blue",
+      subTopics:[
+        {name:"Addition",icon:"mdi-plus"},
+        {name:"Subtraction",icon:"mdi-minus"},
+        {name:"Multiplication",icon:"mdi-close"},
+        {name:"Division",icon:"mdi-division"}]
+    },{
+      name: "Non-Integer Arithmetic",
+      description: "Non-Integer arithmetic applies to operators which result in non-whole numbers, such as fractions, decimals, and percentages.",
+      color:"light-green",
+      subTopics:[
+        {name:"Decimals",icon:"mdi-decimal"},
+        {name:"Fractions",icon:"mdi-ab-testing"},
+        {name:"Percentages",icon:"mdi-percent"}]
+    },{
+      name: "Miscellaneous Arithmetic",
+      description: "This topic covers more complex/real life arithmetic such as problems involving area, volume, time, and currency.",
+      color:"yellow",
+      subTopics:[
+        {name:"Area",icon:"mdi-square-outline"},
+        {name:"Volume",icon:"mdi-cube-outline"},
+        {name:"Time",icon:"mdi-clock-outline"},
+        {name:"Currency",icon:"mdi-currency-gbp"}]
+    },{
+      name: "Statistics",
+      description: "Focused primarily on basic averaging statistics including mean, median, mode, and range. ",
+      color:"orange",
+      subTopics:[
+        {name:"Mean",icon:"mdi-poll"},
+        {name:"Median",icon:"mdi-align-vertical-center"},
+        {name:"Mode",icon:"mdi-chart-scatter-plot"},
+        {name:"Range",icon:"mdi-arrow-expand-horizontal"}]
+    },{
+      name: "Algebra",
+      description: "Narrowly focused on Linear Algebra; dealing with simplifying expressions, using formulas to compute values, and solving equations.",
+      color:"red",
+      subTopics:[
+        {name:"Simplifying Expressions",icon:"mdi-square-root"},
+        {name:"Using Formulas",icon:"mdi-function-variant"},
+        {name:"Solving Equations",icon:"mdi-thought-bubble"}]
+    }]
+  },
   mutations: {},
   actions: {},
   modules: {}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <v-container fluid>
     <v-row align="center" justify="center">
       <v-col cols="12">
-        <v-card class="mx-auto mt-0" max-width="500">
+        <v-card class="mx-auto mt-0" max-width="800">
           <v-card-title class="mx-auto">
             <span class="font-weight-regular mx-2">TOPICS</span>
             <span class="font-weight-thin">| Select a topic...</span>
           </v-card-title>
           <v-divider></v-divider>
           <v-card-text>
+
             <v-expansion-panels focusable multiple>
               <v-expansion-panel v-for="(topic, index) in topicsList" :key="index">
-                <v-expansion-panel-header class="title font-weight-light">
-                  {{ topic.name }}
+                <v-expansion-panel-header>
+                  <div>
+                    <div :class="'title font-weight-regular ' + topic.color + '--text'">{{ topic.name }}</div>
+                    <div class="font-weight-thin pr-5">{{ topic.description }}</div>
+                  </div>
                 </v-expansion-panel-header>
 
                 <v-expansion-panel-content class="pa-0">
@@ -24,6 +28,7 @@
                     </v-list-item>
                   </v-list>
                 </v-expansion-panel-content>
+
               </v-expansion-panel>
             </v-expansion-panels>
           </v-card-text>
@@ -38,45 +43,7 @@ export default {
   name: "home",
   data(){
     return{
-      topicsList:[{
-        name: "Basic Arithmetic",
-        color:"orange",
-        subTopics:[
-          {name:"Addition",icon:"mdi-plus"},
-          {name:"Subtraction",icon:"mdi-minus"},
-          {name:"Multiplication",icon:"mdi-close"},
-          {name:"Division",icon:"mdi-division"}]
-      },{
-        name: "Non-Integer Arithmetic",
-        color:"orange",
-        subTopics:[
-          {name:"Decimals",icon:"mdi-decimal"},
-          {name:"Fractions",icon:"mdi-ab-testing"},
-          {name:"Percentages",icon:"mdi-percent"}]
-      },{
-        name: "Miscellaneous Arithmetic",
-        color:"orange",
-        subTopics:[
-          {name:"Area",icon:"mdi-square-outline"},
-          {name:"Volume",icon:"mdi-cube-outline"},
-          {name:"Time",icon:"mdi-clock-outline"},
-          {name:"Currency",icon:"mdi-currency-gbp"}]
-      },{
-        name: "Statistics",
-        color:"orange",
-        subTopics:[
-          {name:"Mean",icon:"mdi-poll"},
-          {name:"Median",icon:"mdi-align-vertical-center"},
-          {name:"Mode",icon:"mdi-chart-scatter-plot"},
-          {name:"Range",icon:"mdi-arrow-expand-horizontal"}]
-      },{
-        name: "Algebra",
-        color:"orange",
-        subTopics:[
-          {name:"Simplifying Expressions",icon:"mdi-square-root"},
-          {name:"Using Formulas",icon:"mdi-function-variant"},
-          {name:"Solving Equations",icon:"mdi-thought-bubble"}]
-      }]
+      topicsList: this.$store.state.topics
     };
   }
 };
